0bd44350cd16e40778be354f2d076be9cc350341
Wilson Huang <window012@hotmail.com>
2021-11-26 20:03:12 +0800

    Add project files.

---
+++ Fibonacci/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int fib(int n)
+{
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+	int n;
+	std::cout << "Input Fibonacci Number: ";
+	std::cin >> n;
+	std::cout << "fib(" << n << ")=" << fib(n);
+	return 0;
+}
